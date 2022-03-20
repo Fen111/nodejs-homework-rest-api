f602e9495915ae01f488f5957e76fa5e0c03dddf
Viktor Sinelnykov <Fenya.ua@gmail.com>
2022-03-20 17:38:58 +0200

    test server products

---
+++ data/products.js
@@ -0,0 +1,14 @@
+const products = [
+  {
+    _id: "323sfdsf",
+    name: "iPhone X",
+    price: 34000,
+  },
+  {
+    _id: "5645fgdgsfdsf",
+    name: "GeForce RTX 3090",
+    price: 90000,
+  },
+];
+
+module.exports = products;
---
+++ routes/api/products.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const products = require("../../data/products");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result: products,
+    },
+  });
+});
+
+module.exports = router;
